34d7fe27d7955c9df777189b3f250c14d1061fa0
Daniel Bergey <bergey@teallabs.org>
2024-05-27 20:35:42 -0400

    PLU factorization

--- index.ts
+++ index.ts
@@ -1,23 +1,64 @@
-type voltage = {
+export type voltage = {
     type: "voltage";
     V: number;
 }
 
-type resistor = {
+export type resistor = {
     type: "resistor";
     R: number;
 }
 
-type diode = {
+export type diode = {
     type: "diode";
     Vd: number;
 }
 
-type component = voltage | resistor | diode;
-type node = number;
+export type component = voltage | resistor | diode;
+export type node = number;
 
-type placed = {
+export type placed = {
     c: component;
     p: node;
     q: node;
+}
+
+// A should be a matrix with N rows (first index) and N columns (second index)
+// A is modified in place to contain both L & U
+// the permutation array is returend
+function factor_plu(A: number[][]): number[] {
+    const N = A.length;
+
+    const permutations: number[] = [];
+    for (let i = 1; i <= N; i++) {
+        permutations.push(i);
+    }
+
+    for (let k = 1; k <= N; k++) {
+        // pick pivot for row k
+        let p = 0;
+        let k_swap = k;
+        for (let i = k; k <= N; i++) {
+            if (Math.abs(A[i][k]) > p) {
+                p = Math.abs(A[i][k]);
+                k_swap = i;
+            }
+            if (p == 0) {
+                throw "singular matrix";
+            }
+        }
+        p = permutations[k];
+        permutations[k] = permutations[k_swap];
+        permutations[k_swap] = p;
+
+        // elimination
+        for (let i = 1; i <= N; i++) {
+            // multiplier for each row
+            A[i][k] = A[i][k] / A[k][k];
+            for (let j = k + 1; j <= N; j++) {
+                A[i][j] = A[i][j] - A[i][k] * A[k][j]
+            }
+        }
+    }
+
+    return permutations;
 }
